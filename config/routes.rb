8e95d41343510f483f0fab99bb7827d657a6eb36
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-08-27 02:47:46 +0000

    [Add]public-itemコン

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  scope module: 'public' do
+    resources :items, only: [:index, :show]
+  end
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
