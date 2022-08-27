d92349f452264835cfd5fdfdc34b71ebf852aeeb
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-08-27 11:51:36 +0000

    [Add]配送先更新ページ・機能

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -12,8 +12,17 @@ class Public::AddressesController < ApplicationController
     redirect_to addresses_path
   end
 
+
   def edit
+    @address = Address.find(params[:id])
   end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
 
   private
 
