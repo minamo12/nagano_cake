46d3ea5c38e946b63eff085df05fa053c4c73e54
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-08-27 05:37:22 +0000

    [Add]ユーザ編集ページ記述[Fin]ユーザ編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,6 +5,20 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name,
+    :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number)
   end
 
 end
