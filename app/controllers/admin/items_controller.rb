2cf4df266f33a90f4f09bd21f41e7aa6b57a49ab
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-08-28 11:32:26 +0000

    [Up]管理側商品のステータス変更機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -31,6 +31,6 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id)
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
   end
 end
