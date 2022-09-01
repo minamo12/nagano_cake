cf4919467a4e9bca8047f9f541d3938dac78a761
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-09-01 07:22:05 +0000

    [Add]pry導入[Add]注文情報入力画面途中まで

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,21 +8,17 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
   end
 
-  def create
+  def confirm
     @order = Order.new(order_params)
-    @order.customer_id = current_customer.id
-    @order.save
-    redirect_to orderd_path(@order.id)
+    binding.pry
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:postal_code, :address, :name,
-    :shipping_cost, :total_payment, :payment_method, :status)
+    params.require(:order).permit(:payment_method)
   end
 
 end
