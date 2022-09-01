e2bae8ed62b9d41d2017fac2b396872308774ac7
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-09-01 04:44:22 +0000

    [Add]enum導入

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,28 @@
 class Public::OrdersController < ApplicationController
+
   def new
+    @order = Order.new
   end
 
   def index
   end
 
   def show
+    @order = Order.find(params[:id])
   end
+
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    redirect_to orderd_path(@order.id)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name,
+    :shipping_cost, :total_payment, :payment_method, :status)
+  end
+
 end
