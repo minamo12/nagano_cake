ec20e5c83e654eef9b37a5ece4dee17a78ddf6d9
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-09-02 11:02:47 +0000

    [Add]配送先機能途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,16 +11,25 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    #0 自分の住所
+    if params[:order][:select_address] == "0"
     @order = Order.new(order_params)
-    if :select_address == 0
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
-    elsif :select_address == 1
-      @address = Address.find(params[:order][:address_id])
-    else
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+    #1 登録住所からの選択
+
+    elsif params[:order][:select_address] == "1"
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+
+    else #2 新しい登録先
       @order = Order.new(order_params)
     end
+    binding.pry
   end
 
   private
