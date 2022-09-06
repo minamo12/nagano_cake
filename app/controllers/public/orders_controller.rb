429cf536458b6477d85bd1cf0ec4300cd2e80c37
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-09-06 04:31:26 +0000

    [Add]注文履歴へデータを格納

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -40,17 +40,14 @@ class Public::OrdersController < ApplicationController
     @order.shipping_cost = 800
     if @order.save #注文機能への格納が成功したら詳細への格納を開始
       @cart_items = current_customer.cart_items
-      @order_detail = OrderDetail.new
       @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
       @order_detail.order_id = @order.id
       @order_detail.item_id = cart_item.item_id
       @order_detail.amount = cart_item.amount
       @order_detail.price = cart_item.item.price
       @order_detail.save
       end
-      binding.pry
-      #
-      error
     else
     end
     current_customer.cart_items.destroy_all
