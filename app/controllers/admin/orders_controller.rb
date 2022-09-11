2d7d534d396ef442df1d87eaae70750850077cec
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-09-11 02:37:50 +0000

    [Add]注文履歴詳細の注文・製作ステータス更新機能

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -15,4 +15,16 @@ class Admin::OrdersController < ApplicationController
     @total = 0
   end
 
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,10 +39,11 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-    resources :orders, only: [:index, :show]
+    resources :orders, only: [:index, :show, :update]
     resources :genres, only: [:index, :edit, :create, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
+    resources :order_details, only: [:update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
