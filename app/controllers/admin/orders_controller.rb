41c6ebd2c8d4d8c57e9f06f4a1b872c1dcd88247
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-09-18 11:53:04 +0000

    [Add]管理側注文履歴一覧レイアウト

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
 
   def index
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(10)
     @order_details = OrderDetail.all
     @total_amount = OrderDetail.group(:order_id).select('order_id, sum(amount) AS sum_amount')
     @customers = Customer.all
