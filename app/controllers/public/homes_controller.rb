1006d3b5a53a73df75e11210f92292b6659436ce
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-09-19 11:15:11 +0000

    [Add]会員側トップレイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @items = Item.all
+    @items = Item.order(created_at: :desc).limit(4)
   end
 
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_102101) do
+ActiveRecord::Schema.define(version: 2022_09_19_110558) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -91,6 +91,11 @@ ActiveRecord::Schema.define(version: 2022_08_26_102101) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
